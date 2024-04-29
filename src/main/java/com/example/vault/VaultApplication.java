b905ca141cc857b627a5636ef66dd285e8475ce0
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-04-29 10:23:34 -0400

    Working refresh of database connection New credentials were getting injected, we just didn't see a refresh because there was nothing for the context to refresh

--- src/main/java/com/example/vault/VaultApplication.java
+++ src/main/java/com/example/vault/VaultApplication.java
@@ -3,12 +3,17 @@ package com.example.vault;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.vault.core.lease.SecretLeaseContainer;
 import org.springframework.vault.core.lease.event.SecretLeaseExpiredEvent;
 
+import javax.sql.DataSource;
 import java.util.logging.Logger;
 
 @SpringBootApplication
@@ -21,6 +26,14 @@ public class VaultApplication {
 
 @Configuration
 class VaultConfig {
+
+    // Needs something to refresh
+    @Bean
+    @RefreshScope
+    DataSource dataSource(DataSourceProperties properties) {
+        return DataSourceBuilder.create().url(properties.getUrl()).username(properties.getUsername())
+                .password(properties.getPassword()).build();
+    }
 
     VaultConfig(@Value("${spring.cloud.vault.database.role}") String databaseRole,
                 @Value("${spring.cloud.vault.database.backend}") String databaseBackend,
@@ -37,6 +50,9 @@ class VaultConfig {
             if (vaultCredsPath.equals(event.getSource().getPath())) {
                 if (event instanceof SecretLeaseExpiredEvent) {
                     contextRefresher.refresh();
+                    System.out.println("refreshed credentials");
+                    System.out.println("username:"+ environment.getProperty("spring.datasource.username"));
+                    System.out.println("password:"+environment.getProperty("spring.datasource.password"));
                 }
             }
         });
